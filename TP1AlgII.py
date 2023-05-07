95e6e84e1a83a6a630c82782a4c28e5b2d124344
GustavoTCorrea <94574419+GustavoTCorrea@users.noreply.github.com>
2023-05-07 20:48:45 -0300

    Update TP1AlgII.py
    Corrected version of the algorithm

--- TP1AlgII.py
+++ TP1AlgII.py
@@ -8,13 +8,13 @@ class TreeNode:
 
 class TrieTree:
     def __init__(self):
-        self.root = TreeNode()
+        self.root = TreeNode(0, '', [])
         self.num = 1
-    def compress(self, s, file):
+        
+    def countNum(self, s, file):
         currentNode = self.root
         for x in s:
             wasFound = False
-            #print(currentNode.prefix)
             for y in currentNode.filhos:
                 if x == y.prefix:
                     currentNode = y
@@ -24,23 +24,56 @@ class TrieTree:
                 newNode = TreeNode(self.num, x, [])
                 self.num += 1
                 currentNode.filhos.append(newNode)
+                currentNode = self.root
+        k = (self.num.bit_length() + 7) // 8
+        print(self.num, k)
+        if k > 100:
+            print("The text has too many prefixes, and I am honestly impressed by it.")
+            print("Like, there are more than 5 quintillions of them.")
+            print("I do hope this never needs to get to be printed, and the code will not work as expected.")
+        file.write(k.to_bytes(1, byteorder='big', signed=False))
+        return int(k)
+        
+    def compress(self, s, file, numBPI):
+        self.num = 1
+        currentNode = self.root
+        for i in range(0,len(s)):
+            wasFound = False
+            #print(currentNode.prefix)
+            for y in currentNode.filhos:
+                if s[i] == y.prefix:
+                    currentNode = y
+                    wasFound = True
+                    if i == len(s)-1:
+                        file.write((y.value).to_bytes(numBPI, byteorder='big', signed=False))
+                        file.write(s[i].encode('utf8', 'strict'))
+                    break
+            if wasFound == False:
+                newNode = TreeNode(self.num, s[i], [])
+                #print(self.num)
+                self.num += 1
+                currentNode.filhos.append(newNode)
                 k = currentNode.value
-                file.write(k.to_bytes(4, byteorder='big', signed=False))
-                file.write(x.encode('utf8', 'strict'))
+                #print('(',k, x,')')
+                file.write(k.to_bytes(numBPI, byteorder='big', signed=False))
+                file.write(s[i].encode('utf8', 'strict'))
                 #print(len(currentNode.filhos))
                 currentNode = self.root
                 #print(currentNode.value, currentNode.prefix, len(currentNode.filhos))
-                #for k in currentNode.filhos: print(k.prefix)
+                #for k in currentNode.filhos: print(k.prefix)S
                 
     def decompress(self, file):
         dictionary = {}
         dictionary[0] = [0, '']
+        numBPI = int.from_bytes(file.read(1), 'big') #Number of bytes per int
+        print(numBPI)
         #print(dictionary)
         index = 1;
         decompText = ""
         while True:
-            prefix = file.read(4)
+            prefix = file.read(numBPI)
             prefix = int.from_bytes(prefix, "big")
+            #print(prefix)
             suffix = file.read(1)
             #print(suffix)
             if prefix == b'' or suffix == b'':
@@ -52,7 +85,7 @@ class TrieTree:
                 suffix += file.read(1)
             elif nBytes[0:4] == '1110':
                 suffix += file.read(2)
-            elif nBytes[0:5] == '1110':
+            elif nBytes[0:5] == '11110':
                 suffix += file.read(3)
             suffix = suffix.decode("utf8")
             #print(prefix, suffix)
@@ -67,8 +100,6 @@ class TrieTree:
             suffix = suffix[::-1]
             decompText += suffix
             
-            
-        decompText = decompText[:-1]
         return decompText
             
 
@@ -95,15 +126,17 @@ def main():
         inFile = open(sys.argv[2], "r", encoding="utf8")
         #outFile = open("out.z78", "w")
         text = inFile.read()
-        text += '$'
         if len(sys.argv) == 4:
             outFile = open(sys.argv[3], "wb")
         else: #Means len(sys.argv) = 3
             aux = (sys.argv[2]).split('.')[0]
             aux += '.z78'
             outFile = open(aux, "wb")
-        with outFile as file:
-            T.compress(text, file)
+        helperTrie = TrieTree()
+        #print(T.root.filhos, "a")
+        BPI = helperTrie.countNum(text, outFile)
+        #print(T.root.filhos, "b")
+        T.compress(text, outFile, BPI)
         return
     elif sys.argv[1] =='-x':
         if len(sys.argv) == 4:
